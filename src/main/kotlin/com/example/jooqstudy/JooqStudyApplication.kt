8f191f497d6da160ff077cd7b8252ea8ba32c4da
jeong-gumin <jgm@paytalab.com>
2023-09-30 21:30:37 +0900

    init project

---
+++ src/main/kotlin/com/example/jooqstudy/JooqStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jooqstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JooqStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<JooqStudyApplication>(*args)
+}
